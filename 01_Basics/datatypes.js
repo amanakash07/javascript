76a0657cdedfe699732cf2e5793d8a51dbc72f17
Aman Akash <107156810+amanakash07@users.noreply.github.com>
2023-12-08 17:34:04 +0000

    container config done

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,28 @@
+"use Strict"; // treat all js code as newer version //
+
+// alert(3+3) // we are using node js not browser. 
+
+console.log(4+4) 
+console.log("Aman Akash")
+
+let name = "Aman"
+let age = 23
+let isLoggedIn = false
+let state;
+
+// Numbers => 2 to power 53 
+// bigint => large value of numbers, used by trading platform or facebook, reddit etc.
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined 
+// symbol => unique
+// object
+
+console.log (typeof "Aman"); // it will give type of datatype (typeof)
+
+console.log (typeof 23);
+
+console.log (typeof undefined); // output is: undefined.
+
+console.log (typeof null); // it will give output as : Object.
