7c2f1cb2f657a90733a2386de84fac2c416f321a
Aman Akash <107156810+amanakash07@users.noreply.github.com>
2023-12-10 00:29:57 +0530

    conversion Operation in datatype
    conver

---
+++ 01_Basics/conversionOperation.js
@@ -0,0 +1,37 @@
+// let score = 45
+// console.log(typeof score);   //number
+// console.log(typeof (score)); //number
+
+// let score = "45"
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); //number
+
+
+// let score = "45abc"
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); //number
+
+let score = "45abc"
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "45" => 33
+// "45abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = 1 
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);  // true
+
+// 1=> true; 0=> false.
+
+// "" => false
+// "Aman" => true
+
+let someNumber = 45
+let StringNumber = String(someNumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
