1013e24e8b28d8fc96c5862e1ee6d150c8981d6b
Aman Akash <107156810+amanakash07@users.noreply.github.com>
2023-12-11 18:03:02 +0000

    conversion operation in js

--- 01_Basics/conversionOperation.js
+++ 01_Basics/conversionOperation.js
@@ -11,10 +11,10 @@
 // let valueInNumber = Number(score)
 // console.log(typeof valueInNumber); //number
 
-let score = "45abc"
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let score = "45abc"
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "45" => 33
 // "45abc" => NaN
@@ -22,16 +22,58 @@ console.log(valueInNumber);
 // null => 0
 // undefined => NaN
 
-let isLoggedIn = 1 
-let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);  // true
+// let isLoggedIn = 1 
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);  // true
 
 // 1=> true; 0=> false.
 
 // "" => false
 // "Aman" => true
 
-let someNumber = 45
-let StringNumber = String(someNumber);
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// let someNumber = 45
+// let StringNumber = String(someNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+//*******************************************************************************************************/
+
+// let value =5
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "Hello"
+// let str2 = " Aman!"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log(3 + 54 * 5 % 3);  // 5
+
+// console.log(true);  // true
+// console.log(+true); // 1
+
+// let num1 , num2, num3
+// num1 = num2 = num3 = 3+3
+// console.log(num1)
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// Note: google and read:  prefix and postfix in javascript MDN
+
