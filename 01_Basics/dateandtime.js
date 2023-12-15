383ddeb158a4b55ffc98942a182db4a9177fdd7e
Aman Akash <107156810+amanakash07@users.noreply.github.com>
2023-12-15 18:07:12 +0000

    Date and Time in js

---
+++ 01_Basics/dateandtime.js
@@ -0,0 +1,6 @@
+let myDate = new Date ()
+console.log(myDate.toString()); //Fri Dec 15 2023 18:04:59 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toDateString()); // Fri Dec 15 2023
+console.log(myDate.toLocaleString()); //12/15/2023, 6:06:00 PM
+console.log(typeof myDate); //object
+
